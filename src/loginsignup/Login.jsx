0c935de002b27074c9e1a6d0e82ab193f80dc292
AkashThakur14 <ak9988385532@gmail.com>
2025-06-26 14:24:11 +0530

    normal changes

--- src/loginsignup/Login.jsx
+++ src/loginsignup/Login.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Login = () => {
+
+    const userDetail ={
+        email:"",
+        password:""
+    }
+
+const [data, setData]=useState('userDetail');
+
+const handelLoginInput = (e)=> {
+    e.preventDefault();
+   console.log(e.target.value)
+   console.log(e.target.name)
+   console.log(e.target.password)
+
+}
+
     return (
         <div className='login-container'>
+
             <form className='login-form'>
                 <div className='login-header'>
                     <h1>Log In</h1>
@@ -10,8 +27,8 @@ export const Login = () => {
                 </div>
 
                 <div className='login-fields'>
-                    <input type="email" name="email" id="email" placeholder='Email' />
-                    <input type="password" name="password" id="password" placeholder='Password' />
+                    <input type="email" name="email" id="email" placeholder='Email'   onChange={handelLoginInput}/>
+                    <input type="password" name="password" id="password" placeholder='Password'   onChange={handelLoginInput}/>
                     <button type='submit'>Log In</button>
                 </div>
 
@@ -19,6 +36,7 @@ export const Login = () => {
                     <p>Don't have an account? <a href="/signup">Sign Up</a></p>
                 </div>
             </form>
+
         </div>
     )
 }
